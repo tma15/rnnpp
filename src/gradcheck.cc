a94f13d5338c1aad99d9893f866f604b4ca35320
tma15 <takuyamakino15@gmail.com>
2018-03-19 22:26:13 +0900

    Test: update gradient check

--- src/gradcheck.cc
+++ src/gradcheck.cc
@@ -1,4 +1,6 @@
+#include <cmath>
 #include <iostream>
+#include <math.h>
 
 #include "gradcheck.h"
 #include "node.h"
@@ -15,19 +17,12 @@ void gradient_check(Expression &expr) {
 
   Graph* g = expr.g_;
 
-//  std::vector<int> args = g->node(expr.id())->args;
-//  for (int i=0; i < args.size(); ++i) {
-//    std::cout << "inputs " << args[i] << ":" << std::endl;
-//    std::cout << g->outputs[args[i]] << std::endl;
-//  }
-//  std::cout << "output:" << std::endl;
-//  std::cout << g->outputs[expr.id()] << std::endl;
-
   float alpha = 5e-3;
+  bool failed = false;
 
   for (int i=0; i < g->parameter_nodes().size(); ++i) {
-    std::cout << "Grad " << i << std::endl;
-    std::cout << g->grads[i] << std::endl;
+//    std::cout << "Grad " << i << std::endl;
+//    std::cout << g->grads[i] << std::endl;
 
     int nid = g->parameter_nodes()[i];
     int tensor_size = g->outputs[nid].dim.size();
@@ -37,19 +32,31 @@ void gradient_check(Expression &expr) {
 
       g->outputs[nid].data[j] += alpha;
       float e_p = as_scalar(expr.forward());
+//      std::cout << "xp:" << g->outputs[nid].data[j] << std::endl;
 
       g->outputs[nid].data[j] -= 2. * alpha;
       float e_m = as_scalar(expr.forward());
+//      std::cout << "xm:" << g->outputs[nid].data[j] << std::endl;
 
       g->outputs[nid].data[j] = old;
 
       float grad = (e_p - e_m) / (2 * alpha);
       float grad2 = g->grads[nid].data[j];
-//      std::cout << "e_p:" << e_p << " e_m:" << e_m << std::endl;
-      std::cout << "calc:" << grad << " backp:" << grad2 << std::endl;
-//      std::cout << std::endl;
-    }
 
+      float diff = fabs(grad - grad2);
+      float m = std::max(fabs(grad), fabs(grad2));
+      if (diff > 0.01 && m > 0.f) {
+        diff /= m;
+      }
+      if (diff > 0.01 || std::isnan(diff)) {
+        failed = true;
+      }
+//      std::cout << "calc:" << grad << " backp:" << grad2 << std::endl;
+    }
+  }
+
+  if (failed) {
+    std::cerr << "failed to gradient_check" << std::endl;
   }
 }
 
